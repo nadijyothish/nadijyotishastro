e34e765f24aefacf2ea611ed11539531273b15ad
nadijyothish <nadijyothishastro@gmail.com>
2025-10-27 21:37:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -37,4 +37,42 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     });
+
+});
+
+
+// Number Counter Animation
+const counters = document.querySelectorAll('.counter');
+const speed = 80; // smaller number = faster count
+
+const animateCounters = () => {
+  counters.forEach(counter => {
+    const updateCount = () => {
+      const target = +counter.getAttribute('data-target');
+      const current = +counter.innerText;
+
+      const inc = Math.ceil(target / speed);
+
+      if (current < target) {
+        counter.innerText = current + inc;
+        setTimeout(updateCount, 20);
+      } else {
+        counter.innerText = target;
+      }
+    };
+    updateCount();
+  });
+};
+
+// Trigger when visible on screen
+let started = false;
+window.addEventListener('scroll', () => {
+  const section = document.querySelector('.seventhpart');
+  const position = section.getBoundingClientRect().top;
+  const screenHeight = window.innerHeight;
+
+  if (!started && position < screenHeight - 50) {
+    animateCounters();
+    started = true;
+  }
 });
